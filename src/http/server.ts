71045363020cadc89f599573eecd22d9574ca577
joao-pereira1 <jvopereira2004@gmail.com>
2024-09-11 23:12:50 -0300

    Add createGoal router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,48 @@
-import fastify from "fastify";
+import { env } from '../env'
 
-const app = fastify();
+import fastify from 'fastify'
+import z from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { createGoal } from '../functions/create-goal'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+const port = env.PORT
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const {
+			title,
+			desiredWeeklyFrequency
+		} = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
-	.listen({
-		port: 3030,
-	})
-	.then(() => {
-		console.log("HTTP Server Running!");
-	});
+  .listen({
+    port: Number.parseInt(port),
+  })
+  .then(() => {
+    console.log(`Server listening to ${port}`)
+  })
